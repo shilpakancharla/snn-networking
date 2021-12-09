42f74fa1e3f4c20242961314d874421c1521c5ed
Kancharla <skanchar@ndc.nasa.gov>
2021-12-08 21:48:53 -0500

    Creating training loop

--- snn.py
+++ snn.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import numpy as np
 import pandas as pd
@@ -6,13 +7,16 @@ import snntorch as snn
 import matplotlib.pyplot as plt
 from snntorch import utils, spikegen
 from snntorch import spikeplot as splt
-from torch.utils.data import DataLoader
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import MinMaxScaler
 
 class SpikingNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
 
+        number_inputs = 21 # 21 features after feature engineering
+        number_hidden = 1000
+        number_outputs = 3 # 3 targets
         # Initialize layers
         fc1 = nn.Linear(number_inputs, number_hidden) # Applies linear transformation to all input points
         lif1 = snn.Leaky(beta = beta) # Integrates weighted input over time, emitting a spike if threshold condition is met
@@ -38,49 +42,59 @@ class SpikingNeuralNetwork(nn.Module):
 
         return torch.stack(spk2_rec, dim = 0), torch.stack(mem2_rec, dim = 0)
 
+def print_batch_accuracy(data, targets, train = False):
+    output, _ = net(data.view(batch_size, -1))
+    _, idx = output.sum(dim = 0).max(1)
+    acc = np.mean((targets == idx).detach().cpu().numpy())
+
+    if train:
+        print(f"Train set accuracy for a single minibatch: {acc * 100:.2f}%")
+    else:
+        print(f"Test set accuracy for a single minibatch: {acc * 100:.2f}%")
+
+def test_printer():
+    print(f"Epoch {epoch}, Iteration {iter_counter}")
+    print(f"Train Set Loss: {loss_hist[counter]:.2f}")
+    print(f"Test Set Loss: {loss_hist[counter]:.2f}")
+    print_batch_accuracy(data, targets, train = True)
+    print_batch_accuracy(test_data, test_targets, train = False)
+    print("\n")
+
 """
     Process all .csv files to create a dataframe that will be encoded and standardized. Convert this structure to a tensor
     and shape it into the required format for the SNN.
 
     @param csv_filepath: filepath of where all the .csv files are
-    @return tensor of input data in appropriate SNN dimensions
-    @return tensor of output data in appropriate SNN dimensions
+    @param drop_columns: columns that will be dropped from the dataframe before further processing
+    @return numpy array of input data in appropriate SNN dimensions
+    @return numpy array of output data in appropriate SNN dimensions
 """
-def process_dataframe(csv_filepath):
-    df = pd.read_csv(csv_filepath)
-    # Outputs of interest: Avg Packet Loss, Avg Utilization, Max Queue Occupancy
-    df_raw = df.drop(['Link Exists', 
-                        'Avg Packet Length', 
-                        'Avg Utilization First', 
-                        'Avg Packet Loss Rate', 
-                        'Avg Port Occupancy',
-                        'Avg Packet Length First'], axis = 1)
-    
-    # Encode the Time Distribution column
-    le_time = LabelEncoder()
-    df_raw['Time Distribution Encoded'] = le_time.fit_transform(df_raw['Time Distribution'])
-    df_raw = df_raw.drop(['Time Distribution'], axis = 1)
-    
-    # Encode the Size Distribution column
-    le_size = LabelEncoder()
-    df_raw['Size Distribution Encoded'] = le_size.fit_transform(df_raw['Size Distribution'])
-    df_raw = df_raw.drop(['Size Distribution'], axis = 1)
+def process_dataframe(csv_filepath, drop_columns):
+    frames = []
+    count = 0
+    concat_df = pd.DataFrame() # Initialize an empty dataframe
+    for root, directories, file_list in os.walk(csv_filepath):
+        for file in file_list:
+            print("Processing " + file)
+            temp_df = pd.read_csv(csv_filepath + file)
+            # If the link does not exist, discard the row for now
+            temp_df = temp_df[temp_df['Link Exists'] != False].reset_index(drop = True)
+            # Outputs of interest: Avg Packet Loss, Avg Utilization, Max Queue Occupancy
+            temp_concat_df = temp_df.drop(drop_columns, axis = 1)
+            # Start concatenating the files directly
+            concat_df = pd.concat([concat_df.reset_index(drop = True), temp_concat_df.reset_index(drop = True)], ignore_index = True)
+            count = count + 1 # Keep track of number of files processed
+            print("Processed " + file + ". Compeleted " + str(count) + " of " + str(len(file_list)) + " files.")
 
     # Scale the input data using StandardScaler
     scaler = MinMaxScaler()
-    df_scaled = pd.DataFrame(scaler.fit_transform(df_raw), columns = df_raw.columns)
+    df_scaled = pd.DataFrame(scaler.fit_transform(concat_df), columns = concat_df.columns)
     
-    # Convert dataframe to tensor
-    tensor_obj = torch.tensor(df_scaled.values)
+    # Prepare for a tensor structure
+    input_ = df_scaled.iloc[:, :-3].values # Select every column except last three columns of dataframe
+    output_ = df_scaled.iloc[:, -3:].values # Select only last three columns of dataframe
 
-    # Convert dataset to spiking dataset using rate coding
-    spike_data_rate = rate_coding(tensor_obj, 25, 200)
-        
-    # Put dataframe into tensor structure for feeding into spiking neural network
-    #torch_input_tensor = torch.tensor(df_scaled.iloc[:, :-3].values) # Select every column except last three columns of dataframe
-    #torch_output_tensor = torch.tensor(df_scaled.iloc[:, -3:].values) # Select only last three columns of dataframe
-
-    return 
+    return input_, output_
 
 def rate_coding(tensor, batch_size, number_steps):
     # Create DataLoader object
@@ -112,16 +126,60 @@ def delta_modulation(tensor, batch_size, threshold):
     spike_data = spikegen.delta(data_it, threshold = threshold, off_spike = True)
     return spike_data
 
-def train_validation_test_split(df):
-    return
+def training_one_iteration(loss, data, targets):
+    spk_rec, mem_rec = net(data.view(batch_size, -1))
+    print(mem_rec.size())
+
+    # Initailize the total loss value
+    loss_val = torch.zeros((1), dtype = dtype, device = device)
+
+    # Sum loss at every step
+    for step in range(num_steps):
+        loss_val += loss(mem_rec[step], targets)
+
+    print(f"Training loss: {loss_val.item():.3f}")
+
+    print_batch_accuracy(data, targets, train = True)
+    # clear previously stored gradients
+    optimizer.zero_grad()
+
+    # calculate the gradients
+    loss_val.backward()
+
+    # weight update
+    optimizer.step()
 
 # Driver code
 if __name__ == "__main__":
-    csv_filepath = 'tabular_data/25/results_25_400-2000_325_349.csv'
-    df = process_dataframe(csv_filepath)
+    TRAINING_PATH = 'training\\'
+    TEST_PATH = 'test\\'
+    DROP_COLUMNS = ['Unnamed: 0', 'Time Distribution', 'Size Distribution', 'Link Exists', 'Avg Packet Length', 
+                'Avg Utilization First', 'Avg Packet Loss Rate', 'Avg Port Occupancy', 'Avg Packet Length First']
+    input_train, output_train = process_dataframe(TRAINING_PATH, DROP_COLUMNS)
+    print("Input training tensor: " + str(input_train.size()))
+    print("Output training tensor: " + str(output_train.size()))
+    input_test, output_test = process_dataframe(TEST_PATH, DROP_COLUMNS)
+    print("Input test tensor: " + str(input_test.size()))
+    print("Output test tensor: " + str(output_test.size()))
 
-    # Layer parameters
-    number_inputs = 23
-    number_hidden = 1000
-    number_outputs = 3
-    beta = 0.99
+    batch_size = 128
+
+    # Passing numpy array to to DataLoader
+    train = TensorDataset(input_train, output_train)
+    test = TensorDataset(input_test, output_test)
+    train_loader = DataLoader(dataset = train, batch_size = batch_size, shuffle = True)
+    test_loader = DataLoader(dataset = test, batch_size = batch_size, shuffle = True)
+
+    dtype = torch.float
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    # Load network onto CUDA if available
+    net = SpikingNeuralNetwork().to(device)
+    loss = nn.CrossEntropyLoss() # Softmax of output layer, generate loss at output
+    optimizer = torch.optim.Adam(net.parameters(), lr = 5e-4, betas = (0.9, 0.999))
+
+    # One iteration of training
+    data, targets = next(iter(train_loader))
+    data = data.to(device)
+    targets = targets.to(device)
+
+    training_one_iteration(loss, data, targets)
