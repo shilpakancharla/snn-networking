250954c2a8650d2b49fac3864df7d43b5ffcac43
Kancharla <skanchar@ndc.nasa.gov>
2021-12-09 20:36:51 -0500

    Able to create histograms of average packet loss

--- snn.py
+++ snn.py
@@ -11,19 +11,17 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import MinMaxScaler
 
 class SpikingNeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, number_inputs, number_hidden, number_outputs, beta):
         super().__init__()
-
-        number_inputs = 21 # 21 features after feature engineering
-        number_hidden = 1000
-        number_outputs = 3 # 3 targets
-        beta = 0.95
-
+        self.number_inputs = number_inputs
+        self.number_hidden = number_hidden
+        self.number_outputs = number_outputs
+        self.beta = beta
         # Initialize layers
-        self.fc1 = nn.Linear(number_inputs, number_hidden) # Applies linear transformation to all input points
-        self.lif1 = snn.Leaky(beta = beta) # Integrates weighted input over time, emitting a spike if threshold condition is met
-        self.fc2 = nn.Linear(number_hidden, number_outputs) # Applies linear transformation to output spikes of lif1
-        self.lif2 = snn.Leaky(beta = beta) # Another spiking neuron, integrating the weighted spikes over time
+        self.fc1 = nn.Linear(self.number_inputs, self.number_hidden) # Applies linear transformation to all input points
+        self.lif1 = snn.Leaky(beta = self.beta) # Integrates weighted input over time, emitting a spike if threshold condition is met
+        self.fc2 = nn.Linear(self.number_hidden, self.number_outputs) # Applies linear transformation to output spikes of lif1
+        self.lif2 = snn.Leaky(beta = self.beta) # Another spiking neuron, integrating the weighted spikes over time
 
     def forward(self, x):
         num_steps = 25
@@ -49,6 +47,7 @@ class SpikingNeuralNetwork(nn.Module):
 def print_batch_accuracy(data, targets, train = False):
     output, _ = net(data.view(batch_size, -1))
     _, idx = output.sum(dim = 0).max(1)
+    print(type(idx))
     acc = np.mean((targets == idx).detach().cpu().numpy())
 
     if train:
@@ -95,10 +94,18 @@ def process_dataframe(csv_filepath, drop_columns):
     df_scaled = pd.DataFrame(scaler.fit_transform(concat_df), columns = concat_df.columns)
     
     # Prepare for a tensor structure
-    input_ = df_scaled.iloc[:, :-3].values # Select every column except last three columns of dataframe
-    output_ = df_scaled.iloc[:, -3:].values # Select only last three columns of dataframe
-
+    input_ = df_scaled.iloc[:, :-1].values # Select every column except last three columns of dataframe
+    output_ = df_scaled.iloc[:, -1:].values # Select only last three columns of dataframe
     return input_, output_
+
+def create_output_histogram(numerical_data_array, title):
+    bins = np.linspace(0, 1, 100, endpoint = True)
+    # Average packet loss is [0, 1]
+    _ = plt.hist(numerical_data_array, bins = bins, weights = numerical_data_array)
+    plt.title(title)
+    plt.xlabel("Average Packet Loss")
+    plt.ylabel("Number of Values")
+    plt.show()
 
 def rate_coding(tensor, batch_size, number_steps):
     # Create DataLoader object
@@ -168,7 +175,7 @@ def training_one_iteration(train_loader, dtype, device, optimizer):
         loss_val += loss(mem_rec[step], targets)
 
     print(f"Training loss: {loss_val.item():.3f}")
-    print_batch_accuracy(data, targets, train=True)
+    print_batch_accuracy(data, targets, train = True)
 
 def training_loop(net, train_loader, test_loader, dtype, device, optimizer, loss_val):
     num_epochs = 1
@@ -234,10 +241,14 @@ def training_loop(net, train_loader, test_loader, dtype, device, optimizer, loss
 if __name__ == "__main__":
     TRAINING_PATH = 'training\\'
     TEST_PATH = 'test\\'
-    DROP_COLUMNS = ['Unnamed: 0', 'Time Distribution', 'Size Distribution', 'Link Exists', 'Avg Packet Length', 
-                'Avg Utilization First', 'Avg Packet Loss Rate', 'Avg Port Occupancy', 'Avg Packet Length First']
+    DROP_COLUMNS = ['Unnamed: 0', 'Time Distribution', 'Size Distribution', 'Link Exists', 'Avg Utilization', 'Avg Packet Length', 
+                'Avg Utilization First', 'Avg Packet Loss Rate', 'Avg Port Occupancy', 'Max Queue Occupancy', 'Avg Packet Length First']
     #input_train, output_train = process_dataframe(TRAINING_PATH, DROP_COLUMNS)
     #input_test, output_test = process_dataframe(TEST_PATH, DROP_COLUMNS)
+    #print(input_train.shape)
+    #print(output_train.shape)
+    #print(input_test.shape)
+    #print(output_test.shape)
     
     # Save these numpy arrays to load in again
     #np.save("input_train.npy", input_train)
@@ -252,8 +263,13 @@ if __name__ == "__main__":
     OUTPUT_TEST = 'output_test.npy'
     features_train_tensor = np.load(INPUT_TRAIN)
     target_train_tensor = np.load(OUTPUT_TRAIN)
+    print(len(target_train_tensor))
+    # Bin the output train data
+    #create_output_histogram(target_train_tensor, "Histogram for range of values for training output")
     features_test_tensor = np.load(INPUT_TEST)
     target_test_tensor = np.load(OUTPUT_TEST)
+    # Bin the output test data
+    create_output_histogram(target_test_tensor, "Histogram for range of values for test output")
 
     batch_size = 128
 
@@ -265,10 +281,9 @@ if __name__ == "__main__":
 
     dtype = torch.float
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    # Load network onto CUDA if available
-    net = SpikingNeuralNetwork().to(device)
+    net = SpikingNeuralNetwork(21, 1000, 1, 0.95).to(device) # Load network onto CUDA if available
 
-    loss = nn.MSELoss() # Allows you to train multiple targets
+    loss_function = nn.MSELoss() # Regression mean squared loss
     optimizer = torch.optim.Adam(net.parameters(), lr = 5e-4, betas = (0.9, 0.999))
 
     training_one_iteration(train_loader, dtype, device, optimizer)
